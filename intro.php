f88b8708a8c595ae1579f74edbc94f1902e38a0a
Jap <jeet63240@gmail.com>
2022-01-04 19:57:31 +0530

    Intro page done.

--- intro.php
+++ intro.php
@@ -17,6 +17,24 @@
                     <img src="images/img1.svg" alt="Test">
                 </div>
             </div>
+            <div class="my-skills">
+                <h1>My Skills</h1>
+                <div class="skill-container">
+                    <label for="html">HTML :</label>
+                    <meter id="html" min="0" max="100" low="35" high="65" optimum="95" value="95"></meter><br>
+                    <label for="html">CSS :</label>
+                    <meter id="html" min="0" max="100" low="35" high="65" optimum="95" value="90"></meter><br>
+                    <label for="html">JavaScript :</label>
+                    <meter id="html" min="0" max="100" low="35" high="65" optimum="95" value="80"></meter><br>
+                    <label for="html">Python :</label>
+                    <meter id="html" min="0" max="100" low="35" high="65" optimum="95" value="60"></meter><br>
+                    <label for="html">PHP :</label>
+                    <meter id="html" min="0" max="100" low="35" high="65" optimum="95" value="75"></meter><br>
+                </div>
+                <div class="sk-img">
+                    <img src="images/skill.png" alt="skills">
+                </div>
+            </div>
         </div>
     </div>
 
