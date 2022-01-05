92c1b1ee9b266568c63136b0409a9f06d89a7edb
Jap <jeet63240@gmail.com>
2022-01-05 18:25:01 +0530

    Comment menu buttons from this page.

--- index.php
+++ index.php
@@ -5,25 +5,29 @@
 
 <div class="container">
     <?php include 'sidebar.php'; ?>
-    <div class="main">
-        <div class="info-container">
-            <div class="dev-info">
-                <div class="hello">Hello , I am</div>
-                <div class="name">Jeet Patel</div>
-                <div class="about">Student, Developer, Learner</div>
-                <div class="moreabout">I am student of Bachelor of Computer Applications(BCA) in Kamani Science Collage.I live in Gujarat.
-                    I like to learn new things.
-                </div>
-                <div class="btns">
-                    <a href="/cv.txt" target="_blank"><button>Download CV</button></a>
-                    <a href="/contact.html"><button>Contact Me</button></a>
-                </div>
+    <!-- <div class="main">
+        <div class="menus">
+            <img class="menu" src="images/menu.png" alt="menu" width="25px">
+            <img class="close" src="images/close.png" alt="close" width="25px">
+        </div> -->
+    <div class="info-container">
+        <div class="dev-info">
+            <div class="hello">Hello , I am</div>
+            <div class="name">Jeet Patel</div>
+            <div class="about">Student, Developer, Learner</div>
+            <div class="moreabout">I am student of Bachelor of Computer Applications(BCA) in Kamani Science Collage.I live in Gujarat.
+                I like to learn new things.
             </div>
-            <div class="dev-pic">
-                <img src="images/dev.jpg" alt="Developer">
+            <div class="btns">
+                <a href="cv.txt" target="_blank"><button>Download CV</button></a>
+                <a href="/contact.php"><button>Contact Me</button></a>
             </div>
         </div>
+        <div class="dev-pic">
+            <img src="images/dev.jpg" alt="Developer">
+        </div>
     </div>
+</div>
 </div>
 
 <?php include 'footer.php'; ?>
